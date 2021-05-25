30f82fa33460dcdfa138382f6f4ba42dd2ffed61
Nilesh Gosavi <60322098+nileshgosavi17@users.noreply.github.com>
2021-05-25 20:47:23 +0530

    Update server.js

--- Curde-API/server.js
+++ Curde-API/server.js
@@ -1,4 +1,4 @@
-require('./models/db');
+// require('./models/db');
 
 const express = require('express');
 const path = require('path');
@@ -9,14 +9,14 @@ const Handlebars=require('handlebars')
 
 const employeeController = require('./controllers/employeeController');
 
-var app = express();
-app.use(bodyparser.urlencoded({
+     var app = express();
+    app.use(bodyparser.urlencoded({
     extended: true
 }));
-app.use(bodyparser.json());
-app.set('views', path.join(__dirname, '/views/'));
-app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
-app.set('view engine', 'hbs');
+// app.use(bodyparser.json());
+// app.set('views', path.join(__dirname, '/views/'));
+// app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
+// app.set('view engine', 'hbs');
 
 app.listen(3000, () => {
     console.log('Express server started at port : 3000');
